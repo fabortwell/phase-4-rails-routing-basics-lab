7939934cfc58307399e7773e0553c19ed612841b
user <tre@Brian.local>
2022-07-19 14:16:06 +0300

    commit tree 27bf75f22a5df58b08820942623c0850e3313cd0

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
  get '/students', to: 'students#index'
  get '/students/grades', to: 'students#grades'
- get '/highest-grade', to: 'students#highest_grade'
+ get 'students/highest-grade', to: 'students#highest_grade'
 end
